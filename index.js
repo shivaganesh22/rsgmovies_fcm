e0ce3d3d0924f24a95abd03ea45657d0192663f0
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-05-05 22:50:39 +0530

    fcm

--- index.js
+++ index.js
@@ -65,9 +65,34 @@ async function movierulz_fcm() {
         console.error(error);
     }
 }
+async function ibomma_fcm() {
+    try {
+        const response = await axios.get("https://rsg-movies.vercel.app/api/ibomma/");
+        const data = response.data.movies;
+        const queryResult = await pool.query("SELECT * FROM app_ibomma");
+        const movies = queryResult.rows;
+        for (const i of data) {
+            const found = movies.some(movie => movie.name === i.name);
+            if (found) {
+                sendFCMNotification("IBomma Movie Update", i.name, i.image, '/ibomma/movie?link=' + i.link);
+            }
+        }
+        await pool.query("DELETE FROM app_ibomma");
 
-cron.schedule('*/1 * * * *', () => {
-movierulz_fcm()
+       
+        if (data.length > 0) {
+            const insertQuery = "INSERT INTO app_ibomma (name, image, link) VALUES ($1,$2,$3)";
+            const values = data.map(movie => [movie.name, movie.image, movie.link]);
+            await Promise.all(values.map(value => pool.query(insertQuery, value)));
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+cron.schedule('*/5 * * * *', () => {
+movierulz_fcm();
+ibomma_fcm();
 });
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
